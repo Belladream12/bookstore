14189e90ee499e19a9d18b9ea3bdd59768078cd7
hernanku <akuhernandez@outlook.com>
2022-08-29 17:58:20 -0400

    nexus repo infra

---
+++ infra/nexus-repo/variables.tf
@@ -0,0 +1,59 @@
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-xxxxxxx"
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "application_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "cost_center" {
+  type = string
+}
+
+variable "operating_system" {
+  type = string
+}
+
+variable "additonal_sg_cidrs" {
+    type = list(string)
+    description = "additional cidrs allowed to access rds cluster"
+    default = []
+}
+
+
+# asg vars
+variable "ssh_key_name" {
+  type = string
+}
+
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "instance_root_device_size" {
+  type = number
+}
+
