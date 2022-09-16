74359258a8bb18e13d56deadbe1fc84bc57803dc
Belladream12 <110630298+Belladream12@users.noreply.github.com>
2022-09-16 13:40:12 -0400

    finally correct

--- infra/mysql/main.tf
+++ infra/mysql/main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["100.36.228.184/32"]
+    cidr_blocks = ["108.51.77.31/32"]
   }
 
   egress {
